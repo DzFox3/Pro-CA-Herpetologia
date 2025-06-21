46b6fb34bb59c59c8fb0eb2788cd9a50ee55f307
DiazGab <108848133+DiazGab@users.noreply.github.com>
2025-06-21 18:13:53 -0300

    Circulares, arreglo de links navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // podés agregar otras rutas si tenés más componentes
+];
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled'
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
+
+
 export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+declare var bootstrap: any;
+
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  modalImageSrc: string = '';
 
+  openImageModal(src: string): void {
+    this.modalImageSrc = src; 
+    const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+    modal.show();
+  }
 }
